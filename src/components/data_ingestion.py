96670f59f2aa884113ae7d9c503049e7dd099984
Soham Pande <sohampande58@gmail.com>
2025-11-30 23:23:53 +0400

    data transfromation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 # The primary purpose here is to read the data and store it in our database. 
 @dataclass
@@ -47,4 +49,8 @@ class DataIngestion:
 
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    dt = DataTransformation()
+    dt.initiate_data_transformation(train_data, test_data)
